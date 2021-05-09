766ea34d0bdd6f7cb4563fff1a6dc26eb8cef6f7
Nikothaboss <57292271+Nikothaboss@users.noreply.github.com>
2021-05-09 03:36:01 +0200

    test til trygve

--- JS/all-blogs.js
+++ JS/all-blogs.js
@@ -40,32 +40,36 @@ const container = document.querySelector(".container")
 
 let counter = 0;
 const size = (260 + 80);
+const reset = "translateX(0px)" 
+
 
 nextBtn.addEventListener("click", () =>{
     counter ++ ; 
-    contentToPage.style.transform = "translateX(" + (-size * counter) + "px)";
+    const moveCarousel = `translateX(${-size * counter}px)`
+    contentToPage.style.transform = moveCarousel;
         if(container.clientWidth === 1403 && counter > 8){ 
             counter = 0
-            contentToPage.style.transform = "translateX(0px)"
+            contentToPage.style.transform = reset
         }else if(container.clientWidth === 1060 && counter > 9){ 
             counter = 0
-            contentToPage.style.transform = "translateX(0px)"
+            contentToPage.style.transform = reset
         }else if(container.clientWidth === 720 && counter > 10){ 
             counter = 0
-            contentToPage.style.transform = "translateX(0px)"
+            contentToPage.style.transform = reset
         }else if(container.clientWidth <= 380 && counter > 11){ 
             counter = 0
-            contentToPage.style.transform = "translateX(0px)"
+            contentToPage.style.transform = reset
         }
     console.log( container.clientWidth)
 })
 
 prevBtn.addEventListener("click", () =>{
+    const moveCarousel = `translateX(${-size * counter}px)`
     counter -- ; 
-    contentToPage.style.transform = "translateX(" + (-size * counter)   + "px)";
+    contentToPage.style.transform = moveCarousel;
     if(counter <= 0){
         counter = 0;
-        contentToPage.style.transform = "translateX(0px)";
+        contentToPage.style.transform = reset;
     }
     console.log( counter)
 })
