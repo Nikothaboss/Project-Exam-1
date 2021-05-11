094e3401bcdd5d73a228d665cec8371a73e9021c
Nikothaboss <57292271+Nikothaboss@users.noreply.github.com>
2021-05-11 23:22:25 +0200

    animasjon til hamburgerlinkene

--- JS/hamburgerMenu.js
+++ JS/hamburgerMenu.js
@@ -1,12 +1,21 @@
 const hamburgerIcon = document.querySelector(".hamburger-icon")
 const hamburgerMenu = document.querySelector(".hamburger-menu")
+const hamurgerLinks = document.querySelectorAll(".hamburger-links li")
 const body = document.querySelector("body")
-const header = document.querySelector("header")
 
 hamburgerIcon.addEventListener("click", ()=>{
     hamburgerMenu.classList.toggle("hamburger-menu-active")
     hamburgerIcon.classList.toggle("hamburger-animation")
     body.classList.toggle("overflow-hidden")
+
+    // * Animate links
+    hamurgerLinks.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = "";
+        } else{
+            link.style.animation = `hamburgerLinkFade 0.5s ease forwards ${index / 7 +0.3}s`;
+        }
+    });
   
 })
 
