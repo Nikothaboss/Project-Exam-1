1e28d31b8b1116bd4a82881455d0e4d76da51e93
Nikothaboss <57292271+Nikothaboss@users.noreply.github.com>
2021-05-11 21:30:20 +0200

    animasjon på hamburger icon

--- JS/hamburgerMenu.js
+++ JS/hamburgerMenu.js
@@ -1,12 +1,12 @@
 const hamburgerIcon = document.querySelector(".hamburger-icon")
 const hamburgerMenu = document.querySelector(".hamburger-menu")
 const body = document.querySelector("body")
+const header = document.querySelector("header")
 
 hamburgerIcon.addEventListener("click", ()=>{
     hamburgerMenu.classList.toggle("hamburger-menu-active")
+    hamburgerIcon.classList.toggle("hamburger-animation")
     body.classList.toggle("overflow-hidden")
+  
 })
 
-if(hamburgerMenu.classList === "hamburger-menu hamburger-menu-active"){
-    hamburgerIcon.innerHTML = ""
-}
